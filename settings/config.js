c7e390371d08452627c6fced3842f6ae4753060c
Emon <110675255+EmonCme@users.noreply.github.com>
2022-08-23 11:12:40 +0700

    Add files via upload

--- settings/config.js
+++ settings/config.js
@@ -5,12 +5,12 @@ const chalk = require('chalk')
 global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 
 // setting
-global.ownername ="RafliKunn"
-global.ownernumber = "62895410068780"
-global.botname = "RflBotz Ofc"
+global.ownername ="Emon"
+global.ownernumber = "6285799227173"
+global.botname = "BotMee"
 global.thumbnail = fs.readFileSync("./image/elaina.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
-global.lolkey = 'rafliofcheh' //ganti pakai apikey kalian
+global.lolkey = 'Danwfrostkey' //ganti pakai apikey kalian
 global.limit = {
 		free:20,
 		premium:1000
